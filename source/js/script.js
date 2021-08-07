6c1d4cea81744305a9558b31799aefd4e1d08951
ks123kirill <ks123kirill@gmail.com>
2021-08-07 18:38:54 +0300

    Частично сверстана секция проверки задолженностей

---
+++ source/js/script.js
@@ -0,0 +1,33 @@
+// let getActiveForm =  function () {
+//   const debtCheckButtons = document.querySelector('.debt-check__buttons');
+//   const buttonIndividual = debtCheckButtons.querySelector('.debt-check__button--individual');
+//   const buttonEntity = debtCheckButtons.querySelector('.debt-check__button--entity');
+//   const buttonEnforcementProceeding = debtCheckButtons.querySelector('.debt-check__button--enforcement-proceeding');
+//   const buttonAdministrativeCase = debtCheckButtons.querySelector('.debt-check__button--administrative-case');
+
+//   debtCheckButtons.addEventListener('click', function(evt) {
+//     console.log(evt.target);
+//     let buttonTarget = evt.target;
+//     buttonTarget.classList.add('debt-check__button--active');
+//   })
+
+// }
+
+// getActiveForm();
+
+// let getActiveForm =  function () {
+//   const debtCheckButtons = document.querySelector('.debt-check__buttons');
+//   const checkButtons = debtCheckButtons.querySelectorAll('.debt-check__button');
+
+//   // console.log(checkButtons);
+//   checkButtons.forEach((item) = () => {
+//     console.log(item);
+//   });
+// }
+
+// getActiveForm();
+
+
+
+//////////
+
